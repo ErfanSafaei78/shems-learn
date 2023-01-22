1bafbc61bf839fd52073a78e2dc0ad8b09b86ac7
Erfan Safaei <Erfan.Safaei.78@gmail.com>
2023-01-22 15:00:18 +0330

    feat[store.reducers]: question counter reducer added to reducers.

--- src/store/questionCounter/questionCounter.reducer.js
+++ src/store/questionCounter/questionCounter.reducer.js
@@ -3,9 +3,8 @@ export function questionCounterReducer(state = 0, action) {
     switch (action.type) {
         case "COUNTER/INC":
             return state+1;
-        case "COUNTER/DEC":
-            return state-1;
-    
+        case "COUNTER/CLEAR":
+            return state = 0;
         default:
             return state;
     }
