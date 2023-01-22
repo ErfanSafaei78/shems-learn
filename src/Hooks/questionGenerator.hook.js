b85c25d2b2a39a909b9663eeabcc7953cf94d910
Erfan Safaei <Erfan.Safaei.78@gmail.com>
2023-01-22 18:20:37 +0330

    add comments to questionGenerator hook.

--- src/Hooks/questionGenerator.hook.js
+++ src/Hooks/questionGenerator.hook.js
@@ -1,24 +1,20 @@
 import {randomIndex} from "../utils/randomIndex.util"
 
-export function useQuestionGenerator () {
+export function useQuestionGenerator (words) {
     
-    function generateQuestion(words) {
-        const questionType = Math.random() > 0.5 ? "word" : "translate";
-        const questionObj = randomIndex(words);
-        const question = questionObj[questionType];
-        const validAnswer = questionType === "word" ? questionObj.translate : questionObj.word;
-        const items = []
-        for (let i = 0; i < 3; i++){
-            items.push(questionType === "word" ? randomIndex(words)["translate"] : randomIndex(word)["word"])
-        }
-        return {
-            questionId: questionObj.id,
-            question,
-            items,
-            validAnswer
-        }
+    const questionType = Math.random() > 0.5 ? "word" : "translate"; //انتخاب رندوم بین کلمه یا ترجمه برای صورت سوال
+    const questionObj = randomIndex(words);
+    const question = questionObj[questionType];
+    const validAnswer = questionType === "word" ? questionObj.translate : questionObj.word;
+    //انتخاب سه آیتم رندوم برای گزینه های سوال به نسبت تایپ صورت سوال
+    const items = []
+    for (let i = 0; i < 3; i++){
+        items.push(questionType === "word" ? randomIndex(words)["translate"] : randomIndex(word)["word"])
     }
-
-    return { generateQuestion };
-
+    return {
+        questionId: questionObj.id,
+        question,
+        items,
+        validAnswer
+    }
 }
