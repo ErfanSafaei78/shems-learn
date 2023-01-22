0f8fea286c20709e2fd950b382213e98063ec7eb
Erfan Safaei <Erfan.Safaei.78@gmail.com>
2023-01-22 18:10:27 +0330

    initial answeredQuestions hook modified.

---
+++ src/Hooks/answeredQuestions.hook.js
@@ -0,0 +1,12 @@
+
+export function useAnsweredQuestions () {
+    
+    function addQuestion ({id, question, items, validAnswer, userAnswer}) {
+        
+    }
+    
+    return {
+        addQuestion,
+        getAnsweredList,
+    }
+}
