3793cb4cb3789ec42f81153dcf6e88ae78849e5e
shohreh-zarkesh <80383493+shohreh-zarkesh@users.noreply.github.com>
2023-01-23 11:41:17 +0330

    feat : add answered questions

--- src/Hooks/answeredQuestions.hook.js
+++ src/Hooks/answeredQuestions.hook.js
@@ -1,12 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
 
-export function useAnsweredQuestions () {
-    
-    function addQuestion ({id, question, items, validAnswer, userAnswer}) {
-        
-    }
-    
-    return {
-        addQuestion,
-        getAnsweredList,
-    }
+export function useAnsweredQuestions() {
+  const dispatch = useDispatch();
+  const answeredQuestions = useSelector((state) => state.answeredQuestions);
+
+  function addQuestion(questionObj, userAnswer) {
+    dispatch({
+      type: "ANSWEREDLIST/ADD",
+    });
+
+    localStorage.setItem(
+      "answeredQuestions",
+      JSON.stringify(answeredQuestions)
+    );
+  }
+
+  function getAnsweredList() {
+    return JSON.parse(localStorage.getItem("answeredQuestions")) || [];
+  }
+
+  return {
+    addQuestion,
+    getAnsweredList,
+  };
 }
